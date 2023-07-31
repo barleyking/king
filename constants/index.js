28d593d6af299bb620708d7678a1a7f11fbc1356
Anna King <137642071+barleyking@users.noreply.github.com>
2023-07-31 19:37:52 +0100

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x84CFcb9Ae7D2dc21f8aaC46C999aD2Ede157Cfd5";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
